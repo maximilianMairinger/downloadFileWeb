cfcc8db11db5582e2f2ca9bf00ba052b372b6ba4
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-04-05 15:20:23 +0200

    migration

--- app/src/downloadFileWeb.ts
+++ app/src/downloadFileWeb.ts
@@ -1,7 +1,11 @@
 
 
-export function downloadFileWeb() {
-  
+export function downloadFileWeb(url: string) {
+  let aElem = document.createElement("a")
+  aElem.innerHTML = "Download"
+  aElem.href = url
+  aElem.download = "true"
+  aElem.click()
 }
 
 export default downloadFileWeb
